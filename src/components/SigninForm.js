69030e282a696397b5f2075e4aa53c0aabc04b56
rc0fficial <thunder9905@gmail.com>
2023-06-10 15:26:10 +0500

    update

--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -1,64 +1,84 @@
-import { useState } from 'react';
-import firebase from '../firebase';
-import { useRouter } from 'next/router';
-import { getAuth, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import { useState } from "react";
+import firebase from "../firebase";
+import { useRouter } from "next/router";
+import {
+  getAuth,
+  signInWithPopup,
+  FacebookAuthProvider,
+  GoogleAuthProvider,
+} from "firebase/auth";
 // import { FacebookAuthProvider } from "firebase/auth";
 
 const SigninForm = ({ toggleForm }) => {
   const provider = new FacebookAuthProvider();
-    const router = useRouter();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-  
-    const handleSignIn = async () => {
-      try {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        await firebase.auth().signInWithPopup(provider);
-        // Successful login
-        router.push('/home'); // Redirect to the home page
-      } catch (error) {
-        setError(error.message);
-      }
-    };
-    const handleSignInWithFacebook = async () => {
-      const auth = getAuth();
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          // The signed-in user info.
-          const user = result.user;
-      
-          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-          const credential = FacebookAuthProvider.credentialFromResult(result);
-          const accessToken = credential.accessToken;
-      
-          // IdP data available using getAdditionalUserInfo(result)
-          // ...
-        })
-        .catch((error) => {
-          // Handle Errors here.
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          // The email of the user's account used.
-          const email = error.customData.email;
-          // The AuthCredential type that was used.
-          const credential = FacebookAuthProvider.credentialFromError(error);
-      
-          // ...
-        });
-      };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-  
-      try {
-        await firebase.auth().signInWithEmailAndPassword(email, password);
-        // Successful login
-        router.push('/home'); // Redirect to the home page
-      } catch (error) {
-        setError(error.message);
-      }
-    };
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleSignInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+  const handleSignInWithFacebook = async () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // ...
+      });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      // Successful login
+      router.push("/home"); // Redirect to the home page
+    } catch (error) {
+      setError(error.message);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center mt-10">
@@ -78,15 +98,20 @@ const SigninForm = ({ toggleForm }) => {
           onChange={(e) => setPassword(e.target.value)}
           className="border border-gray-300 p-2 mb-2 rounded"
         />
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
           Sign In
         </button>
         {/* Sign in with Google button */}
-        <button onClick={handleSignIn}>Sign In with Google</button>
-        <button onClick={handleSignInWithFacebook}>Sign In with Facebook</button>
+        <button onClick={handleSignInWithGoogle}>Sign In with Google</button>
+        <button onClick={handleSignInWithFacebook}>
+          Sign In with Facebook
+        </button>
       </form>
       <p className="mt-2">
-        Don't have an account?{' '}
+        Don't have an account?{" "}
         <button
           onClick={toggleForm}
           className="text-blue-500 hover:underline focus:outline-none"
