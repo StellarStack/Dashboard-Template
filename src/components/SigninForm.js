54803559559900f5e2636d88a09690c3348524e1
rc0fficial <thunder9905@gmail.com>
2023-06-10 15:19:52 +0500

    update on fb login

--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import firebase from '../firebase';
 import { useRouter } from 'next/router';
+import { getAuth, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+// import { FacebookAuthProvider } from "firebase/auth";
 
 const SigninForm = ({ toggleForm }) => {
+  const provider = new FacebookAuthProvider();
     const router = useRouter();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -18,6 +21,32 @@ const SigninForm = ({ toggleForm }) => {
         setError(error.message);
       }
     };
+    const handleSignInWithFacebook = async () => {
+      const auth = getAuth();
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          // The signed-in user info.
+          const user = result.user;
+      
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          const credential = FacebookAuthProvider.credentialFromResult(result);
+          const accessToken = credential.accessToken;
+      
+          // IdP data available using getAdditionalUserInfo(result)
+          // ...
+        })
+        .catch((error) => {
+          // Handle Errors here.
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          // The email of the user's account used.
+          const email = error.customData.email;
+          // The AuthCredential type that was used.
+          const credential = FacebookAuthProvider.credentialFromError(error);
+      
+          // ...
+        });
+      };
   
     const handleSubmit = async (e) => {
       e.preventDefault();
@@ -54,6 +83,7 @@ const SigninForm = ({ toggleForm }) => {
         </button>
         {/* Sign in with Google button */}
         <button onClick={handleSignIn}>Sign In with Google</button>
+        <button onClick={handleSignInWithFacebook}>Sign In with Facebook</button>
       </form>
       <p className="mt-2">
         Don't have an account?{' '}
