6ebfa6b4488f3ebe06928c54cd64e78f231dde6a
Mahaz Khan <muhammedmahaz93@gmail.com>
2023-06-10 20:13:05 +0500

    code rebased

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+import triangle from "/public/Assets/triangle.png"
+import homepic from "/public/Assets/home-pic.png"
+
+export default function Dashboard() {
+  return (
+    <div className=''>
+    <div className='bg-black'>
+       <Image className='w-[35px] h-[35px]' src={triangle}/>
+       <Image className='w-[18px] h-[18px]' src={homepic}/>
+       
+    </div>
+
+    </div>
+  )
+}
