3a60f0a6f0df65561d35fade4013920f3ed31a64
Muhammad Awais Khan <awaiskhann830@gmail.com>
2023-06-10 08:37:19 -0700

    login page fixes

--- src/components/SigninForm.js
+++ src/components/SigninForm.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import firebase from '../firebase';
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Logo from '/public/Assets/logo.png'
 
 const SigninForm = ({ toggleForm }) => {
     const router = useRouter();
@@ -32,39 +34,65 @@ const SigninForm = ({ toggleForm }) => {
     };
 
   return (
+    <div className='bg-[#b0d8da] w-[100%] h-[100%] flex justify-between'>
+      <div>
+       <Image className='pt-6' src={Logo} width={120} height={80}/>
+      </div>
+
+      <div className='w-[940px] bg-white h-[100%] '>
+      <div className='flex gap-2 text-gray-400 justify-end pr-32 pt-12 '>
+      <p>English(UK)</p>
+      <svg className='mt-1' width="26" height="19" viewBox="0 0 26 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M13 19L0.875645 0.25L25.1244 0.25L13 19Z" fill="#C4C4C4"/>
+      </svg>
+      </div>
+
     <div className="flex flex-col items-center mt-10">
-      <h2 className="text-2xl font-bold mb-4">Sign In</h2>
+
+      <h2 className="text-3xl font-bold mb-10">Login</h2>
       <form onSubmit={handleSubmit} className="w-64">
+
         <input
           type="email"
-          placeholder="Email"
+          placeholder="Full name"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="border border-gray-300 p-2 mb-2 rounded"
+          className="border-b border-gray-300 p-2 w-[350px] mb-2"
         />
         <input
-          type="password"
-          placeholder="Password"
+          type="Email Address"
+          placeholder="Email Address"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="border border-gray-300 p-2 mb-2 rounded"
-        />
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          className="border-b border-gray-300 p-2 w-[350px] mb-2 rounded"
+        />        <input
+        type="Password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        
+        className="border-b border-gray-300 p-2 w-[350px] mb-2  rounded"
+      />
+
+        <div className='pt-12'>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700  text-white w-[350px] font-bold py-2 px-4 rounded ">
           Sign In
         </button>
+        </div> 
         {/* Sign in with Google button */}
-        <button onClick={handleSignIn}>Sign In with Google</button>
       </form>
-      <p className="mt-2">
+      <p className="mt-2 pt-4  text-gray-400">
         Don't have an account?{' '}
         <button
           onClick={toggleForm}
-          className="text-blue-500 hover:underline focus:outline-none"
+          className="text-blue-500  hover:underline focus:outline-none"
         >
-          Sign up
+          Login
         </button>
       </p>
       {error && <p className="text-red-500 mt-4">{error}</p>}
+    </div>
+    </div>
     </div>
   );
 };
