c4511925c4725c67e411c2f4f5c3a61076baf370
rc0fficial <thunder9905@gmail.com>
2023-06-13 16:39:00 +0500

    update

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,22 +3,15 @@ import Image from "next/image";
 import triangle from "/public/Assets/triangle.png";
 import homepic from "/public/Assets/home-pic.png";
 
-
-
 export const Dashboard = () => {
-
-
   return (
-    <div className="">
+    <div>
       <div className="bg-black">
         <Image className="w-[35px] h-[35px]" src={triangle} alt="triangle" />
         <div>
-          <Image className="w-[18px] h-[18px]" src={homepic} alt="home-pic"/>
+          <Image className="w-[18px] h-[18px]" src={homepic} alt="home-pic" />
         </div>
       </div>
     </div>
   );
 };
-
-
-
