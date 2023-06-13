10545d9a66849d28fc71cc70e9bf7049139f6ba0
rc0fficial <thunder9905@gmail.com>
2023-06-13 16:36:23 +0500

    update

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -11,9 +11,9 @@ export const Dashboard = () => {
   return (
     <div className="">
       <div className="bg-black">
-        <Image className="w-[35px] h-[35px]" src={triangle} />
+        <Image className="w-[35px] h-[35px]" src={triangle} alt="triangle" />
         <div>
-          <Image className="w-[18px] h-[18px]" src={homepic} />
+          <Image className="w-[18px] h-[18px]" src={homepic} alt="home-pic"/>
         </div>
       </div>
     </div>
